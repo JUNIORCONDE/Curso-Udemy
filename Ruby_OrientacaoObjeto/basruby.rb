f4d37d564196ade1ee99d44ae284ba1bc2d3ad34
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-13 09:13:28 -0300

    Initial commit

---
+++ Ruby_OrientacaoObjeto/basruby.rb
@@ -0,0 +1,18 @@
+puts "HELLO WORD"
+puts "DIGITE SEU NOME"
+v1 = gets
+
+puts "O SEU NOME É " + v1.inspect
+
+puts "DIGITE SUA IDADE"
+v2 = gets.chomp
+
+puts "SUA IDADE É " + v2
+
+
+
+
+# se colocar .inspec ele vai verificar oque dentro da variavel e vai aparecer /n
+# se rodar a variavel sem gets.chomp no irb vai aparecer /n
+
+# v1, v2 são variaveis, vc pode criar qualqquer variavel 
