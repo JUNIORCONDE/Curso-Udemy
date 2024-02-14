c00b120de05195e089af1b6df5abcba2185e348b
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-13 21:43:16 -0300

    aula17

--- Ruby_OrientacaoObjeto/Estrutura_Repeticao.rb
+++ Ruby_OrientacaoObjeto/Estrutura_Repeticao.rb
@@ -1,3 +1,12 @@
 ## https://www.udemy.com/course/poo-ruby/learn/lecture/7519974#overview
 ## Estrutura de Repetição 
 
+## puts "JUNIOR CONDE \n" * 50 
+
+## while ( em quanto)
+
+i=1
+ while i <= 50
+    puts "JUNIOR CONDE - #{i}"
+    i += 1
+ end
