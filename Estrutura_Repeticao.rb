0b2e101596add36233d4d9177fed12fc45f065c2
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-19 06:06:00 -0300

    up

--- Estrutura_Repeticao.rb
+++ Estrutura_Repeticao.rb
@@ -7,6 +7,16 @@
 
 i=1
  while i <= 50
-    puts "JUNIOR CONDE - #{i}"
+    puts "JUNIOR CONDE - #{i}" #{1} serve para mostrar indicadores
     i += 1
  end
+
+# QDO CHEGAR NO 50, VAMOS FAZER RETORNAR
+# i == 50un
+
+until i <= 0 # enquanto 50 for maior que zero ele vai fazer essa repetição 
+   puts "Sou Maior que Zero -#{i}"
+   i -= 1
+end
+
+puts "fim"
