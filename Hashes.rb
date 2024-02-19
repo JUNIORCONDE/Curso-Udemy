3bac24b54068b43e5940c8603accba228256b2f7
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-19 06:30:27 -0300

    up

---
+++ Hashes.rb
@@ -0,0 +1,19 @@
+# Estudo de vetores, Arrays
+
+#vetor = [1, 2, 3, 4, 5 ]
+
+# ou podemos escrever assim 
+
+vetor = []
+vetor.push("Diego", "João")
+puts vetor
+
+
+
+=begin
+vetor = [1,2,3,4,5,6,7,8,9,10]
+puts "Digite um nome"
+vetor = gets
+puts vetor
+=end
+
