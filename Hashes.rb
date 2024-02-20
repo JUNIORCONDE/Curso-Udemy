aa9f294e77f992654ab2c126724df1c81f11959a
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-19 21:25:47 -0300

    up

--- Hashes.rb
+++ Hashes.rb
@@ -4,9 +4,9 @@
 
 # ou podemos escrever assim 
 
-vetor = []
-vetor.push("Diego", "João")
-puts vetor
+#vetor = []
+#vetor.push("Diego", "João")
+#puts vetor
 
 
 
@@ -16,4 +16,18 @@ puts "Digite um nome"
 vetor = gets
 puts vetor
 =end
+
+#v = [1,2,3,4,5,6]
+#puts v
+
+
+h = {:a=>"123", :b=>"456"} # são duas formas de escrever
+#h = {"a":"123", "b":"456"}
+puts h.class 
+ 
+puts h[:a], h[:b] # isso é para acessar a class hash
+
+
+
+
 
