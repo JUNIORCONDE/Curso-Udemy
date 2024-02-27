02b02fa3b066c16b41a94a16a027e70d6a5aa6a2
JUNIORCONDE <rubenscondejr@uol.com.br>
2024-02-26 22:31:45 -0300

    up2

--- simbolos.rb
+++ simbolos.rb
@@ -15,7 +15,28 @@ h = {:a => "123", :b => "456"}
 #vamos imprimir na tela
 puts h[:a], h[:b]
 puts h.class
+# ou também pode se usar
+puts " tambem pode ser usar h.each_value do |value|
+  puts value
+end" " para imprirmir apenas o valores"
+h.each_value do |value|
+  puts value
+end
+
 # podemos usar dessas forma empregabilidade do :(simbolo)
+puts " podemos usar o comando puts h.inspect, para imprirmir dessa forma na tela."
+puts h.inspect
 
 k = { c: "123", d: "456"}
-# vamos imprirmir na tela
+# vamos imprirmir na tela, apenas os valores
+k.each_value do |value|
+  puts value
+end
+# vamos imprirmir apenas as chaves
+k.each_key do |key|
+  puts key
+end
+# e para imprimir as chaves e valores
+k.each do |key, value|
+  puts "#{key}: #{value}"
+end
